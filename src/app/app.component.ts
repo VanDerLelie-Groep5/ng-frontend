470923c409bc54f1fab65c479a355a23fc07c412
ilias.boujibar@hotmail.com <ilias.boujibar@hotmail.com>
2023-02-11 10:49:52 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Groep5_Van_Der_Lelie_Ui';
+}
